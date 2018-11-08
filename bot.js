9fcff494ab6286f2a89dd7bd655a9d6763ad29ea
Tyler D'Auria <tdauria1@gmail.com>
2018-11-08 14:02:59 +0100

    think its finished

--- bot.js
+++ bot.js
@@ -15,7 +15,6 @@ const bot = new Discord.Client({
   autorun: true,
 });
 
-
 function raidDay(string) {
   const dateFormat = /^(?:(?:(?:0?[13578]|1[02])(\/|-|\.)31)\1|(?:(?:0?[1,3-9]|1[0-2])(\/|-|\.)(?:29|30)\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:0?2(\/|-|\.)29\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:(?:0?[1-9])|(?:1[0-2]))(\/|-|\.)(?:0?[1-9]|1\d|2[0-8])\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
   const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -32,19 +31,15 @@ function raidDay(string) {
 
   const today = new Date();
   let year = today.getFullYear();
-  let month =+ today.getMonth();
+  let month = today.getMonth();
 
   if (string < today.getDate()) {
-    console.log(string, today.getDate())
-    +month + 2;
-    console.log('this month + 2', month)
-
     if (month >= 12) {
       year++;
       month = 1;
     }
-
-    const d = month + '/' + string + '/' + year
+    const d = month + 2 + '/' + string + '/' + year
+    console.log(d);
     const newDay = new Date(d)
     const dayName = days[newDay.getDay()];
     if (raidDays.indexOf(dayName) > -1) {
@@ -72,6 +67,33 @@ function raidDay(string) {
   }
 };
 
+const checker = setInterval(() => {
+  const today = new Date();
+  let date = today.getDate();
+  while (raidDay(date) === false) {
+    date++;
+  }
+  const day = raidDay(date);
+  const time = today.toLocaleTimeString();
+  console.log(time)
+  if (time == "1:56:00 PM") {
+    fs.readFile('late.json', (err, data) => {
+      console.log('times up')
+      if (err) throw err;
+      info = JSON.parse(data)
+      const entries = info.map(entry => entry);
+      const filtered = entries.filter((attend) => attend.date === day);
+      const attendances = filtered.map(e => e.username + ' ' + e.info + ' ' + e.date + '\n').join(",");
+      const message = attendances.replace(/,/g, '');
+      bot.sendMessage({
+        to: '508236289179516928',
+        message: day + ' ' + 'Status:' + '```' + message + '```',
+      });
+    });
+  }
+}, 1000);
+
+
 bot.on('ready', (event) => {
   logger.info('connected');
   logger.info('logged in as');
@@ -80,13 +102,13 @@ bot.on('ready', (event) => {
 
 bot.on('message', (user, userID, channelID, message, event) => {
   const attendance = [];
+  console.log(userID);
   if (message.substring(0, 1) == '!') {
     let args = message.substring(1).split(' ');
     const cmd = args[0];
     const availability = args[1]
     const date = args[2];
     const status = args.slice(3).join(' ')
-    console.log(args)
     switch (cmd) {
       case 'att':
         if (availability == 'late' || availability == 'absent') {
@@ -107,6 +129,7 @@ bot.on('message', (user, userID, channelID, message, event) => {
             };
             attendance.push(log);
             const data = JSON.stringify(attendance);
+            console.log('date here', data)
             if (!fs.existsSync('late.json')) {
               fs.writeFile("late.json", data, (err) => {
                 if (err) throw err;
@@ -115,9 +138,10 @@ bot.on('message', (user, userID, channelID, message, event) => {
             } else {
               fs.readFile('late.json', (err, data) => {
                 if (err) throw err;
-                info = JSON.parse(data)
-                attendance.push(info[0])
-                const infos =  JSON.stringify(attendance);
+                const info = JSON.parse(data)
+                info.push(log);
+                console.log(info);
+                const infos = JSON.stringify(info);
                 fs.writeFile("late.json", infos, (err) => {
                   if (err) throw err;
                   console.log('entries saved')
@@ -126,16 +150,12 @@ bot.on('message', (user, userID, channelID, message, event) => {
                   to: channelID,
                   message: 'Status recorded!',
                 });
-                bot.deleteMessage({
-                  channelID: channelID,
-                  messageID: event.d.id,
-                });
               })
             }
           }
         } else {
           bot.sendMessage({
-            to: channelID,
+            to: userID,
             message: `Must include !att late or absent`,
           });
         };
@@ -143,36 +163,62 @@ bot.on('message', (user, userID, channelID, message, event) => {
 
       case 'help':
         bot.sendMessage({
-          to: channelID,
-          message: 'Late: !late <xMinutes/xHr(s)> <date> Absent: !absend <date> List: !list <date>'
+          to: userID,
+          message:
+            ` Help:
+          !att <(late / absent)> <date> <status>: adds late or absent to the list
+          !clear <date>: clears attendance status for said date
+          !list: shows all attendance info
+          !list <date>: shows all attendance for said date
+          `
         });
         break;
 
-        case 'clear':
+      case 'clear':
         fs.readFile('late.json', (err, data) => {
           const date = args[1];
           const day = raidDay(date);
-          info = JSON.parse(data)
-          console.log(info)
-          const removeThis = info.filter(entry => entry.date == day);
-          console.log(removeThis);
+          const info = JSON.parse(data)
+          info.splice(info.findIndex(entry => entry.date === day && entry.user === userID), 1);
+          const infos = JSON.stringify(info);
+          fs.writeFile("late.json", infos, (err) => {
+            if (err) throw err;
+            console.log('entries saved')
+          })
         });
         break;
 
       case 'list':
         fs.readFile('late.json', (err, data) => {
           if (err) throw err;
+          const date = args[1];
           info = JSON.parse(data)
           const entries = info.map(entry => entry);
-          entries.map(entry => (entry));
-          const attendances = entries.map(e => "<@" + e.user + ">" + '```' + e.info + ' ' + e.date + '\n' + '```').join(",");
-          const message = attendances.replace(/,/g, '');
-          bot.sendMessage({
-            to: channelID,
-            message: message,
-          });
+          if (date) {
+            const day = raidDay(date);
+            const filtered = entries.filter((attend) => attend.date === day);
+            const attendances = filtered.map(e => "```" + e.username + ' ' + e.info + ' ' + e.date + '\n' + '```').join(",");
+            const message = attendances.replace(/,/g, '');
+            bot.sendMessage({
+              to: userID,
+              message: message,
+            });
+          } else {
+            const attendances = entries.map(e => "```" + e.username + ' ' + e.info + ' ' + e.date + '\n' + '```').join(",");
+            const message = attendances.replace(/,/g, '');
+            bot.sendMessage({
+              to: userID,
+              message: message,
+            });
+          };
         });
         break;
-      }
+    }
+  }
+  if(userID != "508236504728993792") {
+    bot.deleteMessage({
+      channelID: channelID,
+      messageID: event.d.id,
+    });
   }
 });
